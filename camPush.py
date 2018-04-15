1ae3e5d350b358ea1c1341bf385c016f9165b931
rakashawn <35199653+rakashawn@users.noreply.github.com>
2018-04-15 10:43:55 -0600

    Create camPush.py

---
+++ camPush.py
@@ -0,0 +1,26 @@
+#from https://github.com/themagpimag/essentials-camera/blob/master/Chapter%2006%20-%20Stop-motion%20and%20selfies/ch6listing1.py
+#importing the necessary modules
+from datetime import datetime
+from gpiozero import Button
+import picamera
+import time
+
+b=Button(14)
+pc=picamera.PiCamera()
+running = True
+#pc.resolution = (1024, 768)
+#use this to set the resolution if you dislike the default values
+timestamp=datetime.now()
+def picture():
+	pc.capture('pic'+str(timestamp)+'.jpg') #taking the picture
+
+pc.start_preview() #running the preview
+b.when_pressed=picture
+try:
+	while running:
+		print('Active')#displaying 'active' to the shell
+		time.sleep(1)
+#we detect Ctrl-C then quit the program
+except KeyboardInterrupt:
+	pc.stop_preview()
+	running = False
